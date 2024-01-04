0517f7d61d709ce3758c32d626ddb39e46da398e
Asad Subhani <asadsubhani77@gmail.com>
2024-01-04 20:55:35 +0500

    Practiced Shadcn UI for pre built components

---
+++ ecom-template-shadcn/src/app/page.tsx
@@ -0,0 +1,12 @@
+import DialogFlie from "@/components/dialogFlie";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center gap-6 p-24">
+      <div>Hello</div>
+      <div>
+        <DialogFlie />
+      </div>
+    </main>
+  );
+}
